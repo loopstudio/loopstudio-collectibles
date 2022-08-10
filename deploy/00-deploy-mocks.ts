80299d5aade4d938c7d23c7bafdfaab5bc529a80
Marcos Martínez <mmartinezciompi@gmail.com>
2022-08-09 21:25:54 -0300

    Removed unnecesary mockV3Aggregator. Change uris sizes to 3 for test purposes

--- deploy/00-deploy-mocks.ts
+++ deploy/00-deploy-mocks.ts
@@ -2,11 +2,7 @@ import { HardhatRuntimeEnvironment } from "hardhat/types";
 import { DeployFunction } from "hardhat-deploy/types";
 import { network } from "hardhat";
 
-import {
-  developmentChains,
-  DECIMALS,
-  INITIAL_ANSWER,
-} from "../helper-hardhat-config";
+import { developmentChains } from "../helper-hardhat-config";
 
 const BASE_FEE = "250000000000000000";
 const GAS_PRICE_LINK = 1e9;
@@ -23,12 +19,6 @@ const func: DeployFunction = async function (hre: HardhatRuntimeEnvironment) {
       from: deployer,
       log: true,
       args: [BASE_FEE, GAS_PRICE_LINK],
-    });
-
-    await deploy("MockV3Aggregator", {
-      from: deployer,
-      log: true,
-      args: [DECIMALS, INITIAL_ANSWER],
     });
 
     log("Mocks deployed..");
--- helper-hardhat-config.ts
+++ helper-hardhat-config.ts
@@ -7,7 +7,6 @@ export const networkConfig: { [key: number]: Record<string, any> } = {
   },
   4: {
     name: "rinkeby",
-    ethUsdPriceFeedAddress: "0x8A753747A1Fa494EC906cE90E9f37563A8AF630e",
     subscriptionId: 9245,
     vrfCoordinatorAddress: "0x6168499c0cFfCaCD319c818142124B7A15E857ab",
     keyHash:
@@ -16,7 +15,6 @@ export const networkConfig: { [key: number]: Record<string, any> } = {
   },
   137: {
     name: "polygon",
-    ethUsdPriceFeedAddress: "0xF9680D99D6C9589e2a93a78A04A279e509205945",
     confirmations: 6,
 
     // TODO: Add Polygon data for LoopNFT
@@ -27,5 +25,3 @@ export const networkConfig: { [key: number]: Record<string, any> } = {
 };
 
 export const developmentChains = ["hardhat", "localhost"];
-export const DECIMALS = 8;
-export const INITIAL_ANSWER = 200000000000;
